0d4507a359d544f65c842751599ab6aa6f7b20c9
MrNerdie12 <56668115+MrNerdie12@users.noreply.github.com>
2019-11-04 12:02:32 +0800

    Create Trees.java

---
+++ Midterms/Trees.java
@@ -0,0 +1,12 @@
+public class Trees {
+
+   public static void main (String [] args) {
+
+    System.out.println("   X");
+    System.out.println("  XXX");
+    System.out.println(" XXXXX");
+    System.out.println("XXXXXXX");
+    System.out.println("   X");
+
+  }
+}
