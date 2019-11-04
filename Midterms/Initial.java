e2cfc1b00fe78ceb68f5dcbdb2ad626084631a34
MrNerdie12 <56668115+MrNerdie12@users.noreply.github.com>
2019-11-04 12:01:28 +0800

    Create Initial.java

---
+++ Midterms/Initial.java
@@ -0,0 +1,12 @@
+public class Initial {
+
+  public static void main (String [] args) {
+
+    System.out.println("IIIII\tM   M\t QQQQ");
+    System.out.println("  I  \tMM MM\tQ    Q");
+    System.out.println("  I  \tM M M\tQ    Q");
+    System.out.println("  I  \tM   M\tQ  Q Q");
+    System.out.println("IIIII\tM   M\t QQQQ Q");
+
+  }
+}
