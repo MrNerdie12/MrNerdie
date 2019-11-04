eeefde11bf52dc61685e7817a97f7fdfbcb5ebfc
MrNerdie12 <56668115+MrNerdie12@users.noreply.github.com>
2019-11-04 12:01:56 +0800

    Create MyInfo.java

---
+++ Midterms/MyInfo.java
@@ -0,0 +1,12 @@
+public class MyInfo {
+
+  public static void main (String [] args) {
+
+    System.out.println("Name:\t\tIan Angelo M. Quitaleg");
+    System.out.println("Course:\t\tBS IT");
+    System.out.println("Year:\t\t1");
+    System.out.println("Address:\tMarcos Highway");
+    System.out.println("Adjective:\tCute");
+
+  }
+}
